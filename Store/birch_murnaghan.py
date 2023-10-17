f20e7a0b1c1b82b2e6f92602d5f486a340c59ca5
Photonico <LukeNiu@outlook.com>
2023-10-18 00:58:16 +1100

    Update

--- Store/birch_murnaghan.py
+++ Store/birch_murnaghan.py
@@ -1,22 +1,23 @@
-# Birch-Murnaghan equation of state
+#### Birch-Murnaghan equation of state
+# pylint: disable = C0103, C0114, C0116, C0301, R0914
 
 import numpy as np
 from scipy.optimize import leastsq
 
 def birch_murnaghan_eos(params, vol):
     """Birch-Murnaghan equation of state."""
-    E0, B0, Bp, V0 = params 
+    E0, B0, Bp, V0 = params
     eta = (vol/V0)**(1.0/3.0)
     E = E0 + 9.0*B0*V0/16.0 * (eta**2-1.0)**2 * (6.0 + Bp*(eta**2-1.0) - 4.0*eta**2)
     return E
 
 def fit_eos(lattice_values, energy_values):
     """Fit energy vs volume data using Birch-Murnaghan equation of state."""
-    
+
     # Convert input lists to numpy arrays if they are not already
     lattice_values = np.array(lattice_values)
     energy_values = np.array(energy_values)
-    
+
     # Assuming volume is the cube of the lattice parameter.
     # Modify as per the specifics of your data and system.
     vol = lattice_values**3
@@ -31,9 +32,9 @@ def fit_eos(lattice_values, energy_values):
 
     # Least squares fitting
     objective_func = lambda params, y, x: y - birch_murnaghan_eos(params, x)
-    params, ier = leastsq(objective_func, initial_params, args=(energy_values, vol))
+    params = leastsq(objective_func, initial_params, args=(energy_values, vol))
 
     # Calculate fitted energy values using optimized parameters
     fitted_energies = birch_murnaghan_eos(params, vol)
-    
+
     return fitted_energies
---
+++ Store/output.py
@@ -0,0 +1,5 @@
+#### Output settings
+# pylint: disable = C0103, C0114, C0116, C0301, C0321, R0913, R0914
+
+def image_setting(length, wide, dpi):
+    return (length, wide), dpi
