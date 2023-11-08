13df3561f786d6210f83aaa4c98acfd85099e8d2
Photonico <LukeNiu@outlook.com>
2023-11-09 09:40:34 +1100

    Update

--- vmatplot/bandgap.py
+++ vmatplot/bandgap.py
@@ -1,19 +1,28 @@
-# Read bandgap from bandstructure calculation
+# Extract bandgap from bandstructure calculation
 # pylint: disable = C0103, C0114, C0116, C0301, C0321, R0914
 
-def read_bandgap(outcar_path):
+import xml.etree.ElementTree as ET
+import os
+import numpy as np
+
+def extract_bandgap_outcar(directory):
     # Check if the user asked for help
-    if outcar_path == "help":
+    if directory == "help":
         print("Please use this function to OUTCAR of the bandstructure calculation.")
         return "Help provided."
 
+    outcar_path = os.path.join(directory, "OUTCAR")
+    if not os.path.isfile(outcar_path):
+        print("OUTCAR file not found in the provided directory.")
+        return None
+
     with open(outcar_path, "r", encoding="utf-8") as outcar_file:
         content = outcar_file.readlines()
-
     # Get HOMO, LUMO, and NKPT
     homo = None
     lumo = None
     nkpt = None
+
     for line in content:
         if "NELECT" in line:
             homo = int(float(line.split()[2]) / 2)
@@ -26,13 +35,13 @@ def read_bandgap(outcar_path):
     lumo_energies = [float(line.split()[1]) for i, line in enumerate(content) if f"     {lumo}     " in line]
 
     # Get maximum HOMO and minimum LUMO energy considering nkpt values
-    e1 = sorted(homo_energies[:nkpt])[-1]
-    e2 = sorted(lumo_energies[:nkpt])[0]
-    bandgap = e2 - e1
+    homo = sorted(homo_energies[:nkpt])[-1]
+    lumo = sorted(lumo_energies[:nkpt])[0]
+    bandgap =  lumo - homo
 
-    # print(f"File: {outcar_path}")
+    # print(f"File: {directory}")
     # print(f"HOMO: band: {homo} E= {e1}")
     # print(f"LUMO: band: {lumo} E= {e2}")
     # print(f"Bandgap: {bandgap}")
 
-    return bandgap, e1, e2
+    return bandgap, homo, lumo
--- vmatplot/bandstructure.py
+++ vmatplot/bandstructure.py
@@ -8,42 +8,78 @@ import numpy as np
 from vmatplot.algorithms import transpose_matrix
 
 def extract_high_symlines(directory):
+    """
+    Extracts the high symmetry lines from the KPOINTS file in a VASP calculation directory.
+
+    Args:
+    directory (str): The directory path that contains the VASP KPOINTS file.
+    
+    Returns:
+    tuple: A tuple containing the kpoints format, number of high symmetry lines, 
+           a set of high symmetry points, and a list of limit points for each line.
+    
+    This function opens the KPOINTS file and reads the high symmetry lines specified within it.
+    It checks for the expected format and extracts the high symmetry points and their limits.
+    """
+    # Open and read the KPOINTS file
     with open(os.path.join(directory, "KPOINTS"), "r", encoding="utf-8") as file:
         KPOINTS = file.readlines()
-    file.close()
-    if KPOINTS[2][0] not in ("l","L"):
+    # Check if the KPOINTS file is in line-mode
+    if KPOINTS[2][0] not in ("l", "L"):
         raise ValueError(f"Expected 'L' on the third line of KPOINTS file, got: {KPOINTS[2]}")
-    # The format of KPOINTS
-    kpoints_format = "cartesian" if KPOINTS[3][0] in ["c", "k"] else "reciprocal"
-    # High symmetry points reading
+    # Determine the format of the kpoints (cartesian or reciprocal)
+    kpoints_format = "cartesian" if KPOINTS[3][0] in ["c", "C"] else "reciprocal"
+    # Initialize a set to store unique high symmetry points
     high_symmetry_points = set()
+    # Read the high symmetry points from the KPOINTS file
     for i in range(4, len(KPOINTS)):
         tokens = KPOINTS[i].strip().split()
         if tokens and tokens[-1].isalpha():
             high_symmetry_points.add(tokens[-1])
+    # The number of unique high symmetry lines
     lines = len(high_symmetry_points)
+    # The set of high symmetry points
     sets = high_symmetry_points
-    # print(f"The number of High Symmetry lines is {lines}")
-    # Extracting non-empty lines
-    non_empty_lines = []
-    for line in KPOINTS[4:]:
-        if line.strip():  # Check if the line is not empty
-            non_empty_lines.append(line.split())
-    # Extracting limits
+    # Extract non-empty lines from the KPOINTS file
+    non_empty_lines = [line.split() for line in KPOINTS[4:] if line.strip()]
+    # Extract the start and end points for each high symmetry line
     limits = []
     for i in range(0, len(non_empty_lines), 2):
         start = non_empty_lines[i]
         end = non_empty_lines[i+1]
         limits.append([start, end])
+    # Return the kpoints format, number of lines, set of high symmetry points, and their limits
     return kpoints_format, lines, sets, limits
 
+
 def extract_fermi_outcar(directory):
+    """
+    Extracts the Fermi energy level from the OUTCAR file of a VASP calculation.
+
+    Args:
+    directory (str): The directory path that contains the VASP OUTCAR file.
+
+    Returns:
+    float: The Fermi energy level extracted from the OUTCAR file.
+
+    This function reads through the OUTCAR file line by line until it finds the line that
+    contains the Fermi energy information. It then extracts and returns this value as a float.
+    If the Fermi energy is not found, the function will return None.
+    """
+    # Open and read the OUTCAR file
     with open(os.path.join(directory, "OUTCAR"), "r", encoding="utf-8") as file:
-        for _, line in enumerate(file):
+        # Iterate over each line in the file
+        for line in file:
+            # Check if the line contains the Fermi energy information
             if "Fermi energy" in line:
+                # Extract the Fermi energy value
                 efermi = line.split()[2]
-                # print(f"The Fermi energy is: {efermi} eV")
+                # Return the Fermi energy as a float
                 return float(efermi)
+    # If the function reaches this point, the Fermi energy was not found
+    # It's a good practice to handle this case, e.g., by logging an error or raising an exception
+    # For now, we'll just return None
+    return None
 
 def extract_fermi(directory):
     xml_file = os.path.join(directory, "vasprun.xml")
@@ -56,50 +92,107 @@ def extract_fermi(directory):
                 return fermi_energy
     raise ValueError("Fermi energy not found in vasprun.xml")
 
-def extract_kpointlist_eigenval(directory):
+def extract_kpoints_eigenval(directory):
+    """
+    Extracts k-point coordinates from a VASP EIGENVAL file.
+
+    Args:
+    directory (str): The directory path that contains the VASP EIGENVAL file.
+
+    Returns:
+    numpy.ndarray: An array of k-point coordinates.
+
+    The function reads the EIGENVAL file, which contains the eigenvalues for each k-point and band.
+    It extracts the k-point coordinates from this file and returns them as a NumPy array.
+    """
     # Open the EIGENVAL file
     with open(os.path.join(directory, "EIGENVAL"), "r", encoding="utf-8") as file:
         lines = file.readlines()
     # Initialize the list for k-points
-    kpoins_list = []
-    # Get the total number of bands and k-points
+    kpoints_list = []
+    # Get the total number of bands and k-points from the sixth line of the file
     try:
         num_bands = int(lines[5].split()[2])
         num_kpoints = int(lines[5].split()[1])
     except IndexError as exc:
+        # If the expected format is not found, raise an error
         raise ValueError("The EIGENVAL file does not have the expected format.") from exc
     # Calculate the number of lines in each k-point block (including the k-point line itself)
     block_size = num_bands + 1
     # Iterate over the EIGENVAL file to extract k-point coordinates
+    # The k-point blocks start from line 7 (index 6) and are spaced by the block size
     for i in range(6, 6 + num_kpoints * block_size, block_size):
-        # Extract the k-point coordinates
+        # Extract the k-point coordinates from the first line of each block
         kpoint_line = lines[i].strip().split()
-        if len(kpoint_line) < 4:  # Check if there are enough elements in the line
+        # Check if there are enough elements in the line to represent a k-point
+        if len(kpoint_line) < 4:
             continue  # Skip lines that don't have enough elements
-        kpoint_coords = [float(kpoint_line[j]) for j in range(3)]  # Take the first three values as k-point coordinates
-        kpoins_list .append(kpoint_coords)
+        # Take the first three values as k-point coordinates (ignoring the weight)
+        kpoint_coords = [float(kpoint_line[j]) for j in range(3)]
+        kpoints_list.append(kpoint_coords)
     # Convert the k-point list to a NumPy array for efficiency
-    kpoints_array = np.array(kpoins_list)
+    kpoints_array = np.array(kpoints_list)
     return kpoints_array
 
-def extract_kpointlist(directory):
+def extract_highsym(directory):
+    """
+    Extracts the list of k-point coordinates from the vasprun.xml file of a VASP calculation.
+
+    Args:
+    directory (str): The directory path that contains the VASP vasprun.xml file.
+    
+    Returns:
+    list: A list of k-point coordinates where each k-point is represented as a list of its coordinates.
+    
+    The function parses the vasprun.xml file to find the k-point coordinates used in the calculation.
+    It looks for the 'varray' XML element with the attribute name set to 'kpointlist', which contains
+    the k-point data. Each k-point is then extracted and appended to a list, which is returned.
+    """
+    # Construct the full path to the vasprun.xml file
     xml_file = os.path.join(directory, "vasprun.xml")
+    # Parse the XML file
     tree = ET.parse(xml_file)
+    # Get the root of the XML tree
     root = tree.getroot()
-    # extract kpoint list
+    # Initialize a list to store the k-point coordinates
     kpoints = []
+    # Find all 'v' elements within 'varray' elements that have a 'name' attribute equal to 'kpointlist'
+    # These elements contain the k-point coordinates
     for kpoint in root.findall(".//varray[@name='kpointlist']/v"):
+        # Split the text content of the 'v' element to get the individual coordinate strings
+        # Convert each coordinate string to a float and create a list of coordinates
         coords = [float(x) for x in kpoint.text.split()]
+        # Append the list of coordinates to the kpoints list
         kpoints.append(coords)
+    # Return the list of k-point coordinates
     return kpoints
 
-def extract_kpath_distances(directory):
-    kpoints = extract_kpointlist(directory)
-    # convert kpoint list to energy list
+def extract_kpath(directory):
+    """
+    Calculates the cumulative distances along a path through k-points in reciprocal space.
+
+    Args:
+    directory (str): The directory path that contains the VASP vasprun.xml file.
+
+    Returns:
+    list: A list of cumulative distances for the path through the k-points.
+
+    This function uses the list of k-point coordinates extracted from the vasprun.xml file
+    and computes the Euclidean distance between successive k-points. These distances are
+    then summed cumulatively to provide a measure of the total path length traversed up to
+    each k-point in the list.
+    """
+    # Extract the list of k-point coordinates
+    kpoints = extract_highsym(directory)
+    # Initialize the list for cumulative distances with the starting point (0 distance)
     cumulative_distances = [0]
+    # Iterate over the list of k-points to calculate the path distances
     for i in range(1, len(kpoints)):
+        # Calculate the Euclidean distance between successive k-points
         distance = np.linalg.norm(np.array(kpoints[i]) - np.array(kpoints[i-1]))
+        # Add the distance from the previous total to get the new cumulative distance
         cumulative_distances.append(cumulative_distances[-1] + distance)
+    # Return the list of cumulative distances
     return cumulative_distances
 
 def extract_weight(directory):
@@ -127,40 +220,142 @@ def extract_kpoints_count(directory):
         return None
 
 def extract_bands_count(directory):
-    eigen_lines = extract_eigenvalues_lines(directory)
+    eigen_lines = extract_eigenvalues_bands(directory)
     return len(eigen_lines)
 
+def kpoints_coordinate(directory):
+    kpoints_file = os.path.join(directory, "KPOINTS")
+    high_symmetry_points = {}
+
+    with open(kpoints_file, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        # Assume high symmetry points start from the fifth line in the KPOINTS file
+        for line in lines[4:]:      # High symmetry points coordinates usually start from line 5
+            if line.strip():        # Ignore empty lines
+                parts = line.split()
+                if len(parts) == 4: # A line with coordinates should have four parts
+                    # Assume the coordinates and label are separated by spaces, with the label being the last part
+                    coords = tuple(map(float, parts[:3]))   # Convert the first three parts to float coordinates
+                    label = parts[3]                        # The last part is the label of the high symmetry point
+                    high_symmetry_points[label] = coords
+    return high_symmetry_points
+
+def kpoints_index(directory):
+    # Retrieve the coordinates of the high symmetry points
+    high_symmetry_points = kpoints_coordinate(directory)
+    # Retrieve the list of kpoints
+    kpoint_list = extract_highsym(directory)
+    # Initialize a dictionary to store the indices of the high symmetry points
+    high_symmetry_indices = {}
+    # For each high symmetry point, find the closest kpoint
+    for label, coord in high_symmetry_points.items():
+        # Initialize a minimum distance to a very large number so any actual distance will be smaller
+        min_distance = float('inf')
+        min_index = None
+        # Iterate over the kpoint list to find the kpoint closest to the current high symmetry point coordinates
+        for index, kpoint in enumerate(kpoint_list):
+            # Calculate the Euclidean distance
+            distance = sum((c - k) ** 2 for c, k in zip(coord, kpoint)) ** 0.5
+            # If this distance is the smallest so far, update the minimum distance and index
+            if distance < min_distance:
+                min_distance = distance
+                min_index = index
+        # Store the index of the closest kpoint
+        high_symmetry_indices[label] = min_index
+    return high_symmetry_indices
+
+def kpoints_path(directory):
+    """
+    This function calculates the path distances for high symmetry points in the Brillouin zone.
+    
+    Args:
+    directory (str): The directory path that contains the VASP output files.
+    
+    Returns:
+    dict: A dictionary mapping high symmetry points to their cumulative path distances.
+    
+    The function works by first finding the indices of the high symmetry points in the k-point list.
+    Then, it calculates the cumulative path distance for each k-point. Finally, it creates a dictionary
+    that maps each high symmetry point label to its corresponding path distance.
+    """
+    # Get the indices of high symmetry points in the k-point list
+    high_symmetry_indices = kpoints_index(directory)
+    # Calculate the cumulative path distances for the k-points
+    path = extract_kpath(directory)
+    # Initialize a dictionary to store the high symmetry points and their path distances
+    high_symmetry_paths = {}
+    # Iterate over the high symmetry points and their indices
+    for label, index in high_symmetry_indices.items():
+        # Map the high symmetry point label to its path distance
+        high_symmetry_paths[label] = path[index]
+    # Return the dictionary of high symmetry points and their path distances
+    return high_symmetry_paths
+
 def extract_eigenvalues_kpoints(directory):
+    """
+    Extracts the eigenvalues for each k-point from a VASP vasprun.xml file.
+
+    Args:
+    directory (str): The directory path that contains the VASP vasprun.xml file.
+
+    Returns:
+    list of lists: A matrix where each sublist contains the eigenvalues for a specific k-point.
+
+    This function parses the vasprun.xml file to extract the electronic energy levels (eigenvalues)
+    at each k-point in the reciprocal lattice for the material being studied. These eigenvalues are
+    crucial for analyzing the material's electronic structure, such as plotting band structures.
+    """
+    # Construct the path to the vasprun.xml file and parse it
     xml_file = os.path.join(directory, "vasprun.xml")
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    # Initialize the set of eigenvalues
+    # Initialize a list to store the eigenvalues for each k-point
     eigenvalues_matrix = []
-    # Navigate to the eigenvalues section
+    # Find the eigenvalues section in the XML tree
     eigenvalues_section = root.find(".//eigenvalues")
     if eigenvalues_section is not None:
-        # Find all k-point <set> elements (assuming they are nested within spin <set> elements)
+        # Find all k-point <set> elements within the eigenvalues section
         kpoint_sets = eigenvalues_section.findall(".//set/set/set")
         if kpoint_sets:
-            # Loop over each k-point set
+            # Iterate over each k-point set to extract eigenvalues
             for kpoint_set in kpoint_sets:
                 kpoint_eigenvalues = []
-                # Loop over each band at the current k-point
+                # Iterate over each band's eigenvalue within the current k-point set
                 for r in kpoint_set.findall("./r"):
-                    # Extract the energy value, which is the first number in the <r> tag's text
+                    # The energy eigenvalue is the first number in the <r> tag's text
                     energy = float(r.text.split()[0])
                     kpoint_eigenvalues.append(energy)
-                # Append the list of eigenvalues for the current k-point to the matrix
+                # Append the list of eigenvalues for this k-point to the matrix
                 eigenvalues_matrix.append(kpoint_eigenvalues)
         else:
+            # Handle the case where no k-point <set> elements are found
             print("No k-point <set> elements found in the eigenvalues section.")
     else:
+        # Handle the case where the eigenvalues section is missing
         print("Eigenvalues section not found in the XML file.")
+    # Return the matrix of eigenvalues
     return eigenvalues_matrix
 
-def extract_eigenvalues_lines(directory):
+def extract_eigenvalues_bands(directory):
+    """
+    Extracts and transposes the eigenvalues matrix from a VASP calculation.
+
+    This function first extracts the eigenvalues for each k-point using the
+    extract_eigenvalues_kpoints function. It then transposes the resulting matrix
+    so that each row corresponds to a band and each column corresponds to a k-point.
+
+    Args:
+    directory (str): The directory path that contains the VASP output files.
+
+    Returns:
+    list of lists: A transposed matrix of eigenvalues where each row contains the
+    eigenvalues for a specific band across all k-points.
+    """
+    # Extract the eigenvalues for each k-point
     eigenvalues_matrix = extract_eigenvalues_kpoints(directory)
+    # Transpose the matrix so that bands are rows and k-points are columns
     transposed_eigenvalues_matrix = transpose_matrix(eigenvalues_matrix)
+    # Return the transposed matrix of eigenvalues
     return transposed_eigenvalues_matrix
 
 def rec_to_cart(klist_source, directory, crystal_type):
