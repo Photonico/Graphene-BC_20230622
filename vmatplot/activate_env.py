c3c3ce547a179babf63c9b1c7cf26e965727e987
Photonico <LukeNiu@outlook.com>
2023-11-08 12:44:07 +1100

    Sync

--- vmatplot/activate_env.py
+++
@@ -1,8 +0,0 @@
-#### Activate Python virtual environment
-# pylint: disable = C0103, C0114
-
-import subprocess
-
-activate_env = "conda activate myenv"
-
-subprocess.run(activate_env, shell = True, check = True)
