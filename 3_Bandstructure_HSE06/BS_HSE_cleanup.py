a91ff853855232717d97bb23918efffb16afc2a6
Photonico <LukeNiu@outlook.com>
2023-11-13 14:41:49 +1100

    Update

---
+++ 3_Bandstructure_HSE06/BS_HSE_cleanup.py
@@ -0,0 +1,31 @@
+#### BS cleanup
+# pylint: disable = C0103, C0114, C0116, C0301, C0321, R0913, R0914, R0915, W0612
+
+#%%
+import os
+
+# List of files to keep
+files_to_keep = [
+    "CHGCAR", "INCAR", "KPOINTS", "KPOINTS_OPT", "POSCAR", "POTCAR", "WAVECAR", "vasp_nci.sh", "BS_HSE_cleanup.py"
+]
+
+def cleanup(files):
+    """Clean VASP works"""
+    # Files input
+    # Start from the current directory and search files and directories recursively
+    for dirpath, dirnames, filenames in os.walk(os.getcwd()):
+        if "lattice_source" in dirnames:
+            dirnames.remove("lattice_source")
+        for filename in filenames:
+            if filename not in files:
+                file_path = os.path.join(dirpath, filename)
+                try:
+                    os.remove(file_path)
+                    print(f"Deleted {file_path}")
+                except (FileNotFoundError, PermissionError) as e:
+                    print(f"Error deleting {file_path}: {e}")
+    print("Cleanup complete.")
+
+# cleanup(files_to_keep)
+
+# %%
