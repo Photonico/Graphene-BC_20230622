e0f4822ffa80dee8351615d05f9ab68e8d3f9687
Photonico <LukeNiu@outlook.com>
2024-03-30 05:30:24 +1100

    Sync

--- 4_PDoS/vasp_artemis.sh
+++ 4_PDoS/vasp_artemis.sh
@@ -4,7 +4,7 @@
 #PBS -q defaultQ
 #PBS -o output.txt
 #PBS -j oe
-#PBS -l select=4:ncpus=32:mem=256GB
+#PBS -l select=3:ncpus=32:mem=128GB
 #PBS -l walltime=256:00:00
 #PBS -m ea
 #PBS -M lniu6305@uni.sydney.edu.au
@@ -14,6 +14,6 @@ cd "$PBS_O_WORKDIR"
 module purge
 module load intel-mpi/18.1 hdf5-intel/1.10.6
 
-mpirun -np 128 /project/RDS-FSC-MOSHEMT-RW/vasp6/vasp_std
+mpirun -np 96 /project/RDS-FSC-MOSHEMT-RW/vasp6/vasp_std
 E=`tail -1 OSZICAR`
 echo $E >> ENERGY.dat
