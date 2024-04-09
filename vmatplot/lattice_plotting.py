d6ca57c655134c2bbe81a23b3ec0d55daa4a9a4b
Photonico <LukeNiu@outlook.com>
2024-04-09 21:07:39 +1000

    Finish lattice plotting

--- vmatplot/lattice_plotting.py
+++ vmatplot/lattice_plotting.py
@@ -2,9 +2,96 @@
 # pylint: disable = C0103, C0114, C0116, C0301, C0321, R0914
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-# from vmatplot.algorithms import fit_eos, polynomially_fit_curve
-# from vmatplot.output import canvas_setting, color_sampling
-# from vmatplot.lattice import read_lattice_free_energy_data, read_lattice_free_energy_count, specify_lattice_free_energy
+from vmatplot.algorithms import fit_eos, extract_part
+from vmatplot.output import canvas_setting, color_sampling
+from vmatplot.lattice import read_free_energy_lattice_count
+from vmatplot.lattice import specify_free_energy_lattice
 
-# def extract_lattice_data(data_file):
+# from vmatplot.lattice_plotting import
+
+# testing: BC₃
+
+def create_matters_lattice(lattice_list):
+    # data = create_matters_lattice(lattice_list)
+    # data[0] = current curve label
+    # data[1] = lattice and free energy data for scatter
+        # data[1][0] = scatter lattice values
+        # data[1][1] = scatter free energy
+    # data[2] = source lattice and free energy data
+        # data[1][0] = source lattice values
+        # data[1][1] = source free energy
+    # data[3] = specific lattice and free energy
+        # data[3][0] = specific lattice
+        # data[3][1] = specific free energy
+    # data[4] = color
+    data = []
+    for lattice_dir in lattice_list:
+        if len(lattice_dir) == 3:
+            label, data_path, spec_path = lattice_dir
+            color = "blue"
+            nsamp = "all"
+            lattice_data = read_free_energy_lattice_count(data_path, nsamp)
+            lattice_tot  = read_free_energy_lattice_count(data_path)
+            data.append([label, lattice_data, lattice_tot, spec_path, color])
+        elif len(lattice_dir) == 4:
+            label, data_path, spec_path, color = lattice_dir
+            nsamp = "all"
+            lattice_data = read_free_energy_lattice_count(data_path, nsamp)
+            lattice_tot  = read_free_energy_lattice_count(data_path)
+            data.append([label, lattice_data, lattice_tot, spec_path, color])
+        elif len(lattice_dir) == 5:
+            label, data_path, spec_path, color, nsamp = lattice_dir
+            lattice_data = read_free_energy_lattice_count(data_path, nsamp)
+            lattice_tot  = read_free_energy_lattice_count(data_path)
+            data.append([label, lattice_data, lattice_tot, spec_path, color])
+    return data
+
+def plot_free_energy_lattice(suptitle, lattice_list = None, lattice_range = (None,None)):
+    # help information
+    help_info = "Usage: plot_free_energy_lattice" + \
+                "Use summarize_free_energy_lattice_directory to extract the free energy versus lattice into free_energy_lattice.dat firstly.\n"
+    if suptitle in ["help", "Help"]:
+        print(help_info)
+
+    # figure Settings
+    fig_setting = canvas_setting()
+    plt.figure(figsize=fig_setting[0], dpi = fig_setting[1])
+    params = fig_setting[2]; plt.rcParams.update(params)
+    plt.tick_params(direction="in", which="both", top=True, right=True, bottom=True, left=True)
+    plt.title(f"Free energy versus lattice for {suptitle}"); plt.xlabel(r"Lattice constant (Å)"); plt.ylabel(r"Energy (eV)")
+
+    # plot data
+    lattice_info_set = create_matters_lattice(lattice_list)
+
+    for _, lattice_info in enumerate(lattice_info_set):
+        # current label
+        if lattice_info[0] not in [None, ""]:
+            current_label = f"({lattice_info[0]})"
+        else:
+            current_label = ""
+        # fitted curve
+        selection_fitted = extract_part(lattice_info[2][0],lattice_info[2][1],lattice_range[0],lattice_range[1])
+        fitted_lattice, fitted_free_energy = fit_eos(selection_fitted[0], selection_fitted[1])
+        colors = color_sampling(lattice_info[4])
+        if lattice_info[1] is not None:
+            plt.plot(fitted_lattice, fitted_free_energy, color=colors[1], zorder=1)
+        else:
+            plt.plot(fitted_lattice, fitted_free_energy, color=colors[1], label=f"Fitted curve {current_label}", zorder=1)
+        # scatter
+        if lattice_info[1] is not None:
+            samples_scatter=extract_part(lattice_info[1][0],lattice_info[1][1],lattice_range[0],lattice_range[1])
+            plt.scatter(samples_scatter[0], samples_scatter[1], s=48, fc="#FFFFFF", ec=colors[1], label=f"Source data {current_label}", zorder=1)
+        # demonstrate the minimum free energy and the corresponding lattice
+        selection_source=extract_part(lattice_info[2][0],lattice_info[2][1],lattice_range[0],lattice_range[1])
+        energy_min_index = np.argmin(selection_source[1])       # Find the index of the minimum energy
+        lattice_min = selection_source[0][energy_min_index]     # Retrieve the corresponding lattice value
+        free_energy_min = selection_source[1][energy_min_index] # Retrieve the minimum free energy value
+        plt.scatter(lattice_min, free_energy_min, s=48, fc=colors[2], ec=colors[2], label=f"Source lowest point {current_label}", zorder=1)
+        # specific data
+        if lattice_info[3] not in [None, ""]:
+            selected_lattice, selected_energy = specify_free_energy_lattice(lattice_info[3])
+            plt.scatter(selected_lattice,  selected_energy, s=24, ec=colors[0], fc=colors[0], label=f"Selected data {current_label}", zorder=2)
+
+    plt.legend(loc=fig_setting[4])
