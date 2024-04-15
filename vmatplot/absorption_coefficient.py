07cf04d54cc758a0743adc976c1b138108d5175d
Photonico <LukeNiu@outlook.com>
2024-04-15 15:55:19 +1000

    Update

--- vmatplot/absorption_coefficient.py
+++ vmatplot/absorption_coefficient.py
@@ -1,6 +1,10 @@
 #### Absorption coefficient plotting
 # pylint: disable = C0103, C0114, C0116, C0301, C0321, R0913, R0914
 
+import matplotlib.pyplot as plt
+
+from vmatplot.output import canvas_setting, color_sampling
+from vmatplot.algorithms import process_boundary, extract_part
 from vmatplot.dielectric_function_plotting import create_matters_dielectric_function
 
 # Physical constants
@@ -27,3 +31,16 @@ def plot_absorption_XZ_col(title, absorption_list=None, inplane_boundary=(None, 
                 "\t Outplane photon wavelenght range (Optional). \n"
     if title in ["help", "Help"]:
         print(help_info)
+        # Figure settings
+    fig_setting = canvas_setting(8, 11)
+    params = fig_setting[2]; plt.rcParams.update(params)
+    fig, axs = plt.subplots(2, 1, figsize=fig_setting[0], dpi=fig_setting[1])
+    axes_element = [axs[0], axs[1]]
+
+    # Colors calling
+    annotate_color = color_sampling("Grey")
+    order_labels = ["a","b"]
+
+    # Materials information
+    dataset = create_matters_dielectric_function(dielectric_list)
+    subtitles = ["In-plane", "Out-of-plane"]
