2b70de94063332adf9105658f5a229c76b334ef3
Photonico <LukeNiu@outlook.com>
2024-04-15 15:35:40 +1000

    Update

--- vmatplot/absorption_coefficient.py
+++ vmatplot/absorption_coefficient.py
@@ -3,6 +3,12 @@
 
 from vmatplot.dielectric_function_plotting import create_matters_dielectric_function
 
+# Physical constants
+hbar_ev = 4.135667662e-15
+c_vacuum = 2.99792458e8
+c_vacuum_nm = 2.99792458e17
+pi = 3.141592654
+
 def create_matters_absorption(*args):
     # data = create_matters_dielectric_function(dielectric_list)
     # data[0] = current curve label
@@ -11,3 +17,13 @@ def create_matters_absorption(*args):
     # data[3] = alpha
     # data[4] = linewidth
     return create_matters_dielectric_function(*args)
+
+def plot_absorption_XZ_col(title, absorption_list=None, inplane_boundary=(None, None), outplane_boundary=(None, None)):
+    help_info = "Usage: absorption_XZ" + \
+                "The independent value includes \n" +\
+                "\t title, \n" +\
+                "\t dielectric function data list, \n" +\
+                "\t Inplane photon wavelenght range (Optional), \n" +\
+                "\t Outplane photon wavelenght range (Optional). \n"
+    if title in ["help", "Help"]:
+        print(help_info)
