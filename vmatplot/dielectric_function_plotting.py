f998797069096fadac7a168af0d377681618591f
Photonico <LukeNiu@outlook.com>
2024-04-23 14:22:12 +1000

    Update

--- vmatplot/dielectric_function_plotting.py
+++ vmatplot/dielectric_function_plotting.py
@@ -195,3 +195,8 @@ def plot_dielectric_function_XZ(*args):
     return plot_dielectric_function_XZ_row(*args)
 
 def plot_dielectric_function_XZ_block(title, dielectric_list=None, inplane_energy_boundary=(None, None), outplane_energy_boundary=(None, None)):
+    # Figure settings
+    fig_setting = canvas_setting(16, 12)
+    params = fig_setting[2]; plt.rcParams.update(params)
+    fig, axs = plt.subplots(2, 2, figsize=fig_setting[0], dpi=fig_setting[1])
+    axes_element = [axs[0, 0], axs[0, 1], axs[1, 0], axs[1, 1]]
