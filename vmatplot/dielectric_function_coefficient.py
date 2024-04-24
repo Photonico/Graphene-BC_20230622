740f01b8699840eae1e6392c533edeb176b79294
Photonico <LukeNiu@outlook.com>
2024-04-24 20:04:39 +1000

    Update

--- vmatplot/dielectric_function_coefficient.py
+++ vmatplot/dielectric_function_coefficient.py
@@ -3,14 +3,11 @@
 
 import numpy as np
 
-from vmatplot.absorption_coefficient import cal_absorption_coefficient
-
 # Speed of light in vacuum in meters per second
 c_mps = 299792458
 # Speed of light in vacuum nanometers per second
 c_nm = c_mps * 1e9  # 1e9 is equivalent to 10^9
 
-def cal_dielectric_function_coefficient(frequency,density_energy_real,density_energy_imag):
-    absorption = cal_absorption_coefficient(frequency,density_energy_real,density_energy_imag)
-    coe = np.sqrt(np.sqrt(2)*frequency*absorption)/c_nm
+def cal_dielectric_function_coefficient_full(frequency,density_energy_real,density_energy_imag):
+    coe = (np.sqrt(2)*frequency/c_nm)*(np.sqrt(np.sqrt(np.square(density_energy_real)+np.square(density_energy_imag))-density_energy_real))
     return coe
