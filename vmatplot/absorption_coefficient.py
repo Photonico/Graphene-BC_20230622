4e152ffd730adbdd8c7286fdc898fa7e23d69005
Photonico <LukeNiu@outlook.com>
2024-04-24 19:48:58 +1000

    Update

--- vmatplot/absorption_coefficient.py
+++ vmatplot/absorption_coefficient.py
@@ -1,4 +1,4 @@
-#### Absorption coefficient plotting
+#### Absorption coefficient calculation and plotting
 # pylint: disable = C0103, C0114, C0116, C0301, C0321, R0913, R0914, W0612
 
 import numpy as np
